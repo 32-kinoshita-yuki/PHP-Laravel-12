82e935d529810f34ebeb8ec43b43d768e186de92
”32-kinoshita-yukigit config --global user.email ”git@github.com:32-kinoshita-yuki/sample.git <inuyasya.19950406@gmail.com>
2020-08-09 15:13:19 +0000

    first commit

--- mynews/resources/views/admin/profile/create.blade.php
+++ mynews/resources/views/admin/profile/create.blade.php
@@ -5,9 +5,21 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>My create</title>
+        <title>My プロフィール</title>
+        <!-- PHP/Laravel 11 課題4.profile.blade.phpファイルを読み込む -->
+@extends('profile.blade')
+        <!-- PHP/Laravel 11 課題4.titleとcontentを編集しましょう -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール</h2>
+            </div>
+        </div>
+    </div>
+
     </head>
     <body>
-        <h1>create作成画面</h1>
+        <h1>プロフィール</h1>
+        
     </body>
 </html>
