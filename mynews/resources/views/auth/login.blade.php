7b6e59eae703872aa951d171cff773f7d126e572
32-kinoshita-yuki <68322937+32-kinoshita-yuki@users.noreply.github.com>
2020-08-11 23:59:52 +0900

    Update login.blade.php

--- mynews/resources/views/auth/login.blade.php
+++ mynews/resources/views/auth/login.blade.php
@@ -8,14 +8,14 @@
                     <div class="login-header card-header mx-auto">{{ __('messages.Login') }}</div>
 
                     <div class="login-body card-body">
-                        <form method="POST" action="{{ route('login') }}">
+                        <form method="POST" action="{{ route('messages.login') }}">
                             @csrf
 
                             <div class="form-group row">
                                 <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('messages.E-Mail Address') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                    <input id="email" type="email" class="form-control{{ $errors->has('messages.email') ? ' is-invalid' : '' }}" name="email" value="{{ old('messages.email') }}" required autofocus>
 
                                     @if ($errors->has('email'))
                                         <span class="invalid-feedback">
@@ -29,7 +29,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('messages.Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                    <input id="password" type="password" class="form-control{{ $errors->has('messages.password') ? ' is-invalid' : '' }}" name="password" required>
 
                                     @if ($errors->has('password'))
                                         <span class="invalid-feedback">
@@ -43,7 +43,7 @@
                                 <div class="col-md-6 offset-md-4">
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('messages.Remember Me') }}
+                                            <input type="checkbox" name="remember" {{ old('messages.remember') ? 'checked' : '' }}> {{ __('messages.Remember Me') }}
                                         </label>
                                     </div>
                                 </div>
